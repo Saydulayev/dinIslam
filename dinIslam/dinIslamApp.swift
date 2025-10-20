2ebd68c957a1cb79afc2db4d3e4ee6a19151cfb3
Saydulayev <saydulayev.wien@gmail.com>
2025-10-20 11:01:34 +0200

    Initial Commit

---
+++ dinIslam/dinIslamApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinIslamApp.swift
+//  dinIslam
+//
+//  Created by Saydulayev on 20.10.25.
+//
+
+import SwiftUI
+
+@main
+struct dinIslamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
